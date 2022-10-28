821a47babe322a3a1297a775d26a8e5cf7166890
NDW <cryptondee@gmail.com>
2022-10-28 22:19:37 +0200

    deploy

---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const RPC_URL = process.env.RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: { url: RPC_URL, accounts: [PRIVATE_KEY] },
+  },
+};
